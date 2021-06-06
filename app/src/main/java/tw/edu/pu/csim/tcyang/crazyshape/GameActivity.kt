3c3c68edf269f6ec3f12f503eb32b4518d14ee4b
master <o1083002@gm.pu.edu.tw>
2021-06-07 03:35:12 +0800

    Firebase功能_(巫萬升)

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/GameActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/GameActivity.kt
@@ -7,10 +7,13 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_game.*
 import org.tensorflow.lite.support.image.TensorImage
 import tw.edu.pu.csim.tcyang.crazyshape.ml.Shapes
-
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class GameActivity : AppCompatActivity() {
@@ -58,6 +61,7 @@ class GameActivity : AppCompatActivity() {
                         val c = Canvas(b)
                         handv.draw(c)
                         classifyDrawing(b)
+                        SaveToStorage(b)
                     }
                 }
                 handv.invalidate()
@@ -65,6 +69,33 @@ class GameActivity : AppCompatActivity() {
             }
         })
     }
+
+    fun SaveToStorage(bmp:Bitmap){
+        //將圖片換成byteArray
+        val baos = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val pictData = baos.toByteArray()
+
+        //val filename = "images/pict.jpg"  //設定子節點與檔名
+
+        //根據系統時間設定檔名
+        val sdf = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS")  //定義時間格式
+        val dt = Date()  //取得現在時間
+        val dts = sdf.format(dt) //將目前日期轉為字串
+        val filename = "images/" + dts + ".jpg"  //設定子節點與檔名
+
+        val reference = FirebaseStorage.getInstance().getReference().child(filename)
+        //上傳到Firebase
+        reference.putBytes(pictData)
+            .addOnSuccessListener {
+                Toast.makeText(baseContext, "上傳成功", Toast.LENGTH_SHORT).show()
+            }
+
+            .addOnFailureListener {
+                Toast.makeText(baseContext, "上傳失敗", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 
 
     fun classifyDrawing(bitmap : Bitmap) {
